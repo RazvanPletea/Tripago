423be238aeb57881aaff3e87f65244074dd80db6
RazvanPletea <pletearazvan25@yahoo.com>
2023-09-01 17:16:50 +0300

    legend first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import FetchData from "./components/FetchData";
 import Header from "./components/Header";
+import Legend from "./components/Legend";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Legend />
       <FetchData />
     </div>
   );
---
+++ src/components/Legend.js
@@ -0,0 +1,14 @@
+import "./legend.css";
+
+export default function Legend() {
+  return (
+    <div className="legend-container">
+      <div className="filter">
+        <span>FILLER</span>
+      </div>
+      <div className="legend-item">
+        <span>filler means that you should not watch this</span>
+      </div>
+    </div>
+  );
+}
