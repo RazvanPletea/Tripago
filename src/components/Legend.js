a8eb7b1e1e3d03b9a38c8592afa6502a25a419ea
RazvanPletea <pletearazvan25@yahoo.com>
2023-09-05 15:58:19 +0300

    responsive desgin

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -1,33 +1,32 @@
 import "./legend.css";
 import "../colors.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import sharringanImage from "../images/uchiha-sharingan.gif";
+
 export default function Legend() {
-  const [animationComplete, setAnimationComplete] = useState(false);
+  const [expandedCategory, setExpandedCategory] = useState(null);
 
-  useEffect(() => {
-    const delayTimer = setTimeout(() => {
-      setAnimationComplete(true);
-    }, 4000);
-
-    return () => {
-      clearTimeout(delayTimer);
-    };
-  }, []);
+  const toggleCategory = (category) => {
+    if (expandedCategory === category) {
+      setExpandedCategory(null);
+    } else {
+      setExpandedCategory(category);
+    }
+  };
 
   return (
     <div className="intro-section">
       <div className="website-description">
-        <div className="text-cotainer">
+        <div className="text-container">
           <h2 className="welcome">Welcome to tripago</h2>
           <p> Explore the world of Naruto with us!</p>
           <p>
             Uncover the mysteries of every episode as we organize them into
-            three clear categories:"Manga Canon," "Mixed," and "Filler." With
+            three clear categories: "Manga Canon," "Mixed," and "Filler." With
             our guidance, you'll easily discern which episodes faithfully follow
             the manga's main story, which expand the narrative with
             anime-original content, and which take delightful detours into
-            filler arcs.{" "}
+            filler arcs.
           </p>
 
           <p>Ready to begin? Start your adventure now!</p>
@@ -40,50 +39,50 @@ export default function Legend() {
         />
       </div>
       <div className="legend-container">
-        <div className="filter manga">
+        <div className="filter manga" onClick={() => toggleCategory("manga")}>
           <span>CANNON</span>
         </div>
+        {expandedCategory === "manga" && (
+          <div className="legend-item visible">
+            <span>
+              Manga Canon Episodes: They faithfully follow the manga's main
+              story, maintaining key events, character growth, and the original
+              plot's integrity.
+            </span>
+          </div>
+        )}
+
         <div
-          className={`legend-item ${
-            animationComplete ? "slide-in-from-left" : "hidden"
-          }`}
+          className="filter mixed-legend"
+          onClick={() => toggleCategory("mixed")}
         >
-          <span>
-            Manga Canon Episodes: They faithfully follow the manga's main story,
-            maintaining key events, character growth, and the original plot's
-            integrity.
-          </span>
-        </div>
-        <div className="filter mixed-legend">
           <span>MIXED</span>
         </div>
-        <div
-          className={`legend-item ${
-            animationComplete ? "slide-in-from-left" : "hidden"
-          }`}
-        >
-          <span>
-            {" "}
-            These episodes blend elements from the manga with anime-original
-            content, expanding the story while maintaining the core storyline's
-            integrity.
-          </span>
-        </div>
+        {expandedCategory === "mixed" && (
+          <div className="legend-item visible">
+            <span>
+              These episodes blend elements from the manga with anime-original
+              content, expanding the story while maintaining the core
+              storyline's integrity.
+            </span>
+          </div>
+        )}
 
-        <div className="filter filler-legend">
+        <div
+          className="filter filler-legend"
+          onClick={() => toggleCategory("filler")}
+        >
           <span>FILLER</span>
         </div>
-        <div
-          className={`legend-item ${
-            animationComplete ? "slide-in-from-left" : "hidden"
-          }`}
-        >
-          <span>
-            These episodes stray from the manga's main story, often featuring
-            unrelated or less engaging plots that may not resonate with fans of
-            the original narrative.
-          </span>
-        </div>
+        {expandedCategory === "filler" && (
+          <div className="legend-item visible">
+            <span>
+              These episodes stray from the manga's main story, often featuring
+              unrelated or less engaging plots that may not resonate with fans
+              of the original narrative.
+            </span>
+          </div>
+        )}
       </div>
     </div>
   );
